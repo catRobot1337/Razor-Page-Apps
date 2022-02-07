b8eda415c4b1d67e10183331727716eb12f09961
nice1373 <nice1373@outlook.com>
2022-02-06 22:03:12 -0500

    Added data annotations for our table / db requirements

--- RazorPageApps Project 0/Model/Category.cs
+++ RazorPageApps Project 0/Model/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace RazorPageApps_Project_0.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPageApps_Project_0.Model;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
 
