4e36dec96bf45f568ddd02138bc2b3bde1b097cc
nice1373 <nice1373@outlook.com>
2022-02-06 23:40:20 -0500

    Created db context, implemented with EFC, and prepared ApplicationDbContext for us to add whatever table models we'd like to our project

---
+++ RazorPageApps Project 0/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPageApps_Project_0.Data;
+
+//=============================================================
+//
+// This file implements the Db Context and acts like a data
+// layer that will connect our application with the database.
+//
+// It must implement the Db context clause inside the EFC
+// i.e. public class ApplicationDbContext : DbContext <-- for this
+// to work we must add the following up top
+// using Microsoft.EntityFrameworkCore;
+//
+//=============================================================
+
+
+public class ApplicationDbContext: DbContext
+{
+    // Once we've properly implemented the db context clause inside
+    // the EFC we can add whatever table models we'd like below
+}
