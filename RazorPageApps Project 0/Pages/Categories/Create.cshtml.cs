37299c3a014ec46c931eb49d82e6f6de9580ec50
catRobot1337 <76880436+catRobot1337@users.noreply.github.com>
2022-02-12 18:10:51 -0500

    [DEV] Create /Categories/Create subpage accessed through a button on the /Categories page using form and post method. No adjustment to .cs yet

---
+++ RazorPageApps Project 0/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageApps_Project_0.Pages.Categories;
+
+public class CreateModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
