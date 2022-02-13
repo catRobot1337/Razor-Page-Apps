4322a700ea2a79f652f72b7d8f964e85ad44c976
catRobot1337 <76880436+catRobot1337@users.noreply.github.com>
2022-02-12 19:40:38 -0500

    [DEV] Added Category property/model inside the model of the UI (CreateModel) aka in .cs file. Bound the input text fields for Name and DisplayOrder to the Category model in the cshtml so that the values will be available for the Post to use in the .cs file.

--- RazorPageApps Project 0/Pages/Categories/Create.cshtml.cs
+++ RazorPageApps Project 0/Pages/Categories/Create.cshtml.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPageApps_Project_0.Model;
 
 namespace RazorPageApps_Project_0.Pages.Categories;
 
 public class CreateModel : PageModel
 {
+
+    public Category Category { get; set; }
+
     public void OnGet()
     {
     }
