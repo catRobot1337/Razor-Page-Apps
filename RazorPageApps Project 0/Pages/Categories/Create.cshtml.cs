fbbe6e78c2b4aef8e545c908d4adbbf7c5ae6126
catRobot1337 <76880436+catRobot1337@users.noreply.github.com>
2022-02-12 20:17:43 -0500

    [DEV] Created Category and Bind Property.
    1. Created OnPost handler that receives category object and calls it 'category'.
    2. Added dependency injection with ApplicationDbContext to allow us to work with the database
    2a. Added constructor to retrieve from dependency injection ApplicationDbContext as db and assign it as our local _db var
    3. Fetch Category object through the OnPost handler that we will add to our db as 'category', save db changes, and redirect the page to main Index.cshtml
    
    4. Final update changed how the OnPost handler and Model work. Added [BindProperty] tag to the model so that we can remove redundancy of grabbing Category object and setting as 'category'. Adding the tag will automatically fetch the defined object in the UI cshtml asp-for for us to manipulate thru the OnPost handler

--- RazorPageApps Project 0/Pages/Categories/Create.cshtml.cs
+++ RazorPageApps Project 0/Pages/Categories/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPageApps_Project_0.Data;
 using RazorPageApps_Project_0.Model;
 
 namespace RazorPageApps_Project_0.Pages.Categories;
@@ -7,9 +8,28 @@ namespace RazorPageApps_Project_0.Pages.Categories;
 public class CreateModel : PageModel
 {
 
-    public Category Category { get; set; }
+    private readonly ApplicationDbContext _db;
+
+    public CreateModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    [BindProperty] // Will bind Category object properties with the UI cshtml in asp-for and will populate inside the same 'Category' object
+    public Category Category { get; set; } // Model for Category. Get category object set it as 'Category'
 
     public void OnGet()
     {
+    }
+
+    // In an MVC environment we would create an OnPost() action method.
+    // But in Razor pages, we create handlers.
+    // Handlers always start with the keyword "On" following the HTTP word that the handler will do.
+    // IActionResult returns to some page or redirects to a view
+    public async Task<IActionResult> OnPost()
+    {
+        _db.AddAsync(Category);
+        _db.SaveChanges();
+        return RedirectToPage("Index");
     }
 }
